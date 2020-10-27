828ba7f6ae383dccb820854fecf650fc75fb724b
Sean Harrison <sah@bookgenesis.com>
2020-10-26 22:25:19 -0500

    .

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ do cd $f
     echo $f
     time docker build . -t seanharrison/hello:$f
     cd ..
+    docker run --rm -it seanharrison/hello:$f
 done
 docker image ls | grep seanharrison/hello
 docker push seanharrison/hello
