577ab8d78ce19982d81dc07d7496eba28178efc6
Sean Harrison <sah@kruxia.com>
2021-12-08 05:29:40 -0600

    added sbcl and python, but not in scratch

--- push.sh
+++ push.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 for f in $(ls -d */ | cut -f1 -d'/'); do
-do
     docker push seanharrison/hello:$f
 done
---
+++ sbcl/build.cl
@@ -0,0 +1,2 @@
+(load #P"hello.cl")
+(sb-ext:save-lisp-and-die "hello" :toplevel #'hello :executable t :compression 9)
