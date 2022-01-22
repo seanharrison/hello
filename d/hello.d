ed2d3d4316606c871507a4f28c6dbaa70ac8d09d
Sean Harrison <sah@kruxia.com>
2022-01-22 15:43:20 -0600

    d -- use alpine:3.13, because more recent versions break libunwind.

--- d/hello.d
+++ d/hello.d
@@ -1,5 +1,3 @@
-#!/usr/bin/env rdmd
-
 void main() {
     import std.stdio : writeln;
     writeln("Hello from D!");
--- pull.sh
+++ pull.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for image in nimlang/nim:alpine alpine:latest golang rust:latest alpine:latest debian:bullseye-slim
+for image in nimlang/nim:alpine alpine:3.13 golang rust:latest alpine:latest debian:bullseye-slim
 do 
     docker pull $image
 done
