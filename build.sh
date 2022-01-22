e49da79e3e795a0b0f4ec91042780482c67db7ea
Sean Harrison <sah@kruxia.com>
2022-01-22 16:24:29 -0600

    build.sh - list the docker images by size

--- build.sh
+++ build.sh
@@ -7,4 +7,11 @@ for f in $(ls -d */ | cut -f1 -d'/'); do
     cd ..
     time docker run --rm seanharrison/hello:$f
 done
-docker image ls | grep seanharrison/hello | sort
+# list the images by size
+# https://tunzor.github.io/posts/docker-list-images-by-size/
+docker image ls --format "{{.Size}}\t{{.Repository}}:{{.Tag}}\t{{.ID}}\t{{.CreatedSince}}" \
+    | grep seanharrison/hello \
+    | awk '{if ($1~/MB/) print substr($1, 1, length($1)-2) * 1000 "kB\t" $2 "\t" $3 "\t" $4 " " $5 " " $6 ; else print $1 "\t" $2 "\t" $3 "\t" $4 " " $5 " " $6 }' \
+    | sort -n
+
+
